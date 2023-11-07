a3a973cb5ad1319c383f5d4c7ae52404ba7f6bb8
KimJiYeon <jsujyeon@naver.com>
2023-11-07 19:56:01 +0900

    first_commit

---
+++ src/main/java/com/spring/notebook/EngNotebookApplication.java
@@ -0,0 +1,13 @@
+package com.spring.notebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngNotebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngNotebookApplication.class, args);
+	}
+
+}
