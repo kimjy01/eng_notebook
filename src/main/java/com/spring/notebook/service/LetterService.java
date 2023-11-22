d23a238870e9db7313e09939fa785610f957b156
topping <reyna11@naver.com>
2023-11-22 21:12:32 +0900

    1122 스티커 구분 전까지 작업

---
+++ src/main/java/com/spring/notebook/service/LetterService.java
@@ -0,0 +1,41 @@
+package com.spring.notebook.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.spring.notebook.entity.Letters;
+import com.spring.notebook.repository.LetterRepository;
+
+@Service
+public class LetterService {
+	
+	private LetterRepository letterRepository;
+	
+	@Autowired
+	public LetterService(LetterRepository letterRepository) {
+		this.letterRepository = letterRepository;
+	}
+	
+	public Letters getStickerInfoByLetterId(Long stickerNumber) {
+		// LetterId로 Letter를 조회해 반환
+		return letterRepository.findById((long) stickerNumber)
+				.orElseThrow(() -> new RuntimeException("해당하는 편지가 없어요 (id : " + stickerNumber + ")"));
+	}
+	
+	// 편지 저장
+	public void saveLetter(Letters letter) {
+		letterRepository.save(letter);
+	}
+	
+	// 편지 작성
+	public void write(Letters letter) {
+		letterRepository.save(letter);
+	}
+	
+	// 편지 리스트
+	public List<Letters> letterList() {
+		return letterRepository.findAll();
+	}
+}
